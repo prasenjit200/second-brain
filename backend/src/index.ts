a74515b19765043f60131696732ef31be2dd5746
prasenjitdey <prasenjit.official2004@gmail.com>
2025-03-30 15:15:42 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,12 @@ import { rootRouter } from './routes';
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+    origin: "https://second-brain-self.vercel.app", // Allow only your frontend
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+}));
+
 app.use(express.json());
 
 app.use("/api/v1",rootRouter);
